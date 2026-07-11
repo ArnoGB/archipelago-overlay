c1eadf4ea391b5e4ec7b8482ce53791d402b33c2
ArnoGB <ArnoGB>
2026-07-11 11:54:03 +0200

    added game avatars

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ var AVATAR_IMG = [
   { name: "Hollow Knight", img: "./character/hollow_knight.png" },
   { name: "Dark Souls III", img: "./character/dark_souls_3.png" },
   { name: "Pokemon Emerald", img: "./character/pokemon.png" },
+  { name: "Super Mario 64", img: "./character/super_mario.png" },
+  { name: "Terraria", img: "./character/terraria.png" },
+  { name: "Stardew Valley", img: "./character/stardew_valley.png" },
+  { name: "Ocarina of Time", img: "./character/zelda_ocarina.png" },
+  { name: "Risk of Rain 2", img: "./character/risk_of_rain.png" },
+  { name: "Kingdom Hearts 2", img: "./character/kingdom_hearts.png" },
+  { name: "Starcraft 2", img: "./character/starcraft.png" },
+  { name: "A Link to the Past", img: "./character/zelda_ocarina.png" }, //should be its own pic
+  { name: "Undertale", img: "./character/undertale.png" },
+  { name: "Minecraft", img: "./character/minecraft.png" },
 ];
 var AVATAR_IMG_DEFAULT = "./character/unknown.png";
 
